c4882ec6de1c1157b9479077197c83df81ffefdf
jayedumindu <jayedumindu@gmail.com>
2023-11-15 22:14:48 +0530

    rollback

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,4 @@
 import { NextResponse } from "next/server";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export const middleware = async (request) => {
   const path = request.nextUrl.pathname;
