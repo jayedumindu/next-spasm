718d99ccc578b871f1e3f8e6a565f8549f79479a
jayedumindu <jayedumindu@gmail.com>
2023-11-15 22:13:50 +0530

    middleware changed

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,9 @@
 import { NextResponse } from "next/server";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const middleware = async (request) => {
   const path = request.nextUrl.pathname;
