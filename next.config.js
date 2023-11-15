6e70fa325d51289831c5d7e667ac9399d44cb205
jayedumindu <jayedumindu@gmail.com>
2023-11-15 22:24:12 +0530

    webpack updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "~": __dirname,
+    };
+  },
 };
 
 module.exports = nextConfig;
