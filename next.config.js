8aac224a38db7e150c37bbbd9fdbfbfc155c9baf
jayedumindu <jayedumindu@gmail.com>
2023-11-15 23:11:06 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.alias.react = path.resolve("./node_modules/react");
-    config.node = { __dirname: true };
-    return config;
+  env: {
+    ROOT: __dirname,
   },
 };
 
