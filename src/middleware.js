aa490d175a68c418b1e46a7b26339dfdd7c21516
jayedumindu <jayedumindu@gmail.com>
2023-11-15 22:33:55 +0530

    updated

--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,16 @@
 import { NextResponse } from "next/server";
 
 export const middleware = async (request) => {
-  const path = request.nextUrl.pathname;
-  const token = request.cookies.get("token")?.value || "";
-
-  const isPublicPath =
-    path === "/login" || path === "/signup" || path === "/verifymail";
-
-  if (isPublicPath && token) {
-    return NextResponse.redirect(new URL("/", request.nextUrl));
-  }
-
-  if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL("/login", request.nextUrl));
-  }
+  // const path = request.nextUrl.pathname;
+  // const token = request.cookies.get("token")?.value || "";
+  // const isPublicPath =
+  //   path === "/login" || path === "/signup" || path === "/verifymail";
+  // if (isPublicPath && token) {
+  //   return NextResponse.redirect(new URL("/", request.nextUrl));
+  // }
+  // if (!isPublicPath && !token) {
+  //   return NextResponse.redirect(new URL("/login", request.nextUrl));
+  // }
 };
 
 // See "Matching Paths" below to learn more
