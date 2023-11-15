14a5f9730204137170ce3b30c595a2b4211a1887
jayedumindu <jayedumindu@gmail.com>
2023-11-15 23:56:55 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-var path = require("path");
+
 const nextConfig = {
   reactStrictMode: false,
-  distDir: "out",
-  output: "export",
 };
 
 module.exports = nextConfig;
