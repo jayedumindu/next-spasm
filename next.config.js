0f9c4efaf3ae22ef65346b42b7aa91aafa6c22fe
jayedumindu <jayedumindu@gmail.com>
2023-11-16 00:51:23 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
-    // this includes files from the monorepo base two directories up
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-  },
 };
 
 module.exports = nextConfig;
