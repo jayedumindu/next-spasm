95df1193acb68a8ed98a7a38bde8473c3dd36f5f
jayedumindu <jayedumindu@gmail.com>
2023-11-16 00:42:23 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
 };
 
 module.exports = nextConfig;
--- src/middleware.js
+++ src/middleware.js
@@ -15,6 +15,5 @@ export function middleware(request) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  runtime: "edge",
   matcher: ["/", "/login", "/signup", "/verifymail"],
 };
