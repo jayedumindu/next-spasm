b6afe87639c5dcc6648de1dcb61e111b7b127e66
jayedumindu <jayedumindu@gmail.com>
2023-11-15 23:19:32 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+var path = require("path");
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias["__dirname"] = path.resolve("~");
+    return config;
+  },
 };
 
 module.exports = nextConfig;
